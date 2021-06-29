36bc395e1ee5d50e0bffd589daedc5b8bb68ec0a
santiagoWilli <santiago30mw@gmail.com>
2021-06-29 15:00:28 +0100

    Router mode changed to hash

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import store from "@/store";
 import Home from "@/components/Home";
 import Login from "@/components/Login";
@@ -21,7 +21,8 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
+    mode: 'hash',
     routes
 })
 
